6f725e8a6a5f1553a60c97aaed11aed04f3ae1aa
goforbroke <go.for.broke1006@gmail.com>
2021-04-19 02:26:31 +0300

    staking goroutines sample

---
+++ staking_goroutines/README.md
@@ -0,0 +1,21 @@
+# staking goroutines
+
+```bash
+cd staking_goroutines
+wget -O trace.out http://localhost:8080/debug/pprof/trace?seconds=60
+go tool trace trace.out
+```
+
+Summary
+
+<img src="trace-goroutine-application-Run-func.png"/>
+
+All goroutines spend about 16 seconds. 
+
+4 seconds for sorting random numbers.
+
+12 seconds sleeping.
+
+Red part means "execution time".
+
+Dark grey means "sleep/idle time" I guess.
---
+++ staking_goroutines/app.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+)
+
+func NewDaemonizedTask() *daemonizedTask {
+	return &daemonizedTask{
+		repeatPeriod: 5 * time.Second,
+		stuckPeriod:  12 * time.Second,
+	}
+}
+
+type daemonizedTask struct {
+	repeatPeriod time.Duration
+	stuckPeriod  time.Duration
+
+	stopInit chan struct{}
+	stopDone chan struct{}
+}
+
+func (t daemonizedTask) Run() {
+	ticker := time.NewTicker(t.repeatPeriod)
+
+LOOP:
+	for {
+		select {
+		case <-t.stopInit:
+			break LOOP
+		case <-ticker.C:
+			go t.process()
+		}
+	}
+}
+
+func (t daemonizedTask) Stop() {
+	t.stopInit <- struct{}{}
+	<-t.stopDone
+}
+
+func (t daemonizedTask) process() {
+	const size = 16777216
+
+	var numbers []int
+	for i := 0; i < size; i++ {
+		numbers = append(numbers, rand.Int())
+	}
+
+	sort.Ints(numbers)
+
+	// fake job
+	time.Sleep(t.stuckPeriod)
+}
